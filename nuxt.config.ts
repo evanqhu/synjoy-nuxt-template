0f8cf73e43fe0691704e666fca8e429fa0297eb5
evanqhu <qianghu98@163.com>
2024-10-25 10:10:18 +0800

    chore: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint'],
   devtools: { enabled: true },
+  // srcDir: 'app/',
+  // future: {
+  //   compatibilityVersion: 4, // 启用 Nuxt 4 版本后，目录结构会发生改变；暂时使用 Nuxt 3 版本，不使用 app 或 src 文件夹
+  // },
   compatibilityDate: '2024-04-03',
   eslint: {
     config: {
